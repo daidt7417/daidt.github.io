512249502bb95646887717fd7ecefbb01a8f0e7a
DaiDT7417 <daidt7417@co-well.vn>
2024-08-09 15:31:32 +0700

    ch

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const otpInputs = document.querySelectorAll(".otp-input");
-const otpInput = document.getElementById("otp");
 
-otpInput.addEventListener("change", (e) => {
-  const otp = e.target.value;
-  alert(otp);
-  otpInputs.forEach((input, index) => {
-    input.value = otp[index] && otp[index] !== "?" ? otp[index] : "";
+otpInputs.forEach((current, i) => {
+  current.addEventListener("change", (event) => {
+    const otp = Number(event.target.value);
+    if (otp.length <= 1) return;
+    otpInputs.forEach((input, index) => {
+      input.value = otp || "";
+
+      if (index < otpInputs.length - 1) {
+        otpInputs[index + 1].focus();
+      }
+    });
   });
 });
 
@@ -28,25 +33,20 @@ otpInputs.forEach((current, i) => {
     } else if (current.nextElementSibling) {
       current.nextElementSibling.focus();
     }
-
-    otpInput.value = [...otpInputs].reduce((opt, input) => {
-      return `${opt}${input.value || "?"}`;
-    }, "");
-    otpInput.dispatchEvent(new Event("change"));
   });
 });
 
-window.addEventListener("paste", (event) => {
-  const clipboardData = event.clipboardData || window.clipboardData;
-  const pastedData = clipboardData.getData("text").trim();
+// window.addEventListener("paste", (event) => {
+//   const clipboardData = event.clipboardData || window.clipboardData;
+//   const pastedData = clipboardData.getData("text").trim();
 
-  if (/^\d{4}$/.test(pastedData)) {
-    otpInputs.forEach((input, index) => {
-      input.value = pastedData[index];
+//   if (/^\d{4}$/.test(pastedData)) {
+//     otpInputs.forEach((input, index) => {
+//       input.value = pastedData[index];
 
-      if (index < otpInputs.length - 1) {
-        otpInputs[index + 1].focus();
-      }
-    });
-  }
-});
+//       if (index < otpInputs.length - 1) {
+//         otpInputs[index + 1].focus();
+//       }
+//     });
+//   }
+// });
