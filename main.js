7a499fd38e2c7118012b0534957bc71ef3a8b07e
DaiDT7417 <daidt7417@co-well.vn>
2024-08-09 14:00:38 +0700

    dsadsa

--- main.js
+++ main.js
@@ -16,13 +16,22 @@ otpInputs.forEach((current) => {
     } else if (current.nextElementSibling) {
       current.nextElementSibling.focus();
     }
+
+    if (index === 0) {
+      otpInputs.forEach((input, index) => {
+        input.value = otp[index] || "";
+        if (index < otpInputs.length - 1 && otp[index]) {
+          otpInputs[index + 1].focus();
+        }
+      });
+    }
   });
 });
 
 window.addEventListener("paste", (event) => {
   const clipboardData = event.clipboardData || window.clipboardData;
   const pastedData = clipboardData.getData("text").trim();
-  alert(pastedData)
+  alert(pastedData);
 
   if (/^\d{4}$/.test(pastedData)) {
     otpInputs.forEach((input, index) => {
