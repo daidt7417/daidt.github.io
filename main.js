f4638b7fdfd1db6c527bcbc0ae2f7f01e23ab35d
DaiDT7417 <daidt7417@co-well.vn>
2024-08-09 11:18:34 +0700

    test

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-function moveFocus(current, event) {
-  if (event.inputType === "deleteContentBackward") {
-    if (current.previousElementSibling) {
-      current.previousElementSibling.focus();
-    }
-  } else if (current.nextElementSibling) {
-    current.nextElementSibling.focus();
-  }
-}
 const otpInputs = document.querySelectorAll(".otp-input");
+
+otpInputs.forEach((current) => {
+  current.addEventListener("input", (event) => {
+    const value = Number(event.target.value);
+    if (isNaN(value)) {
+      current.value = "";
+      event.preventDefault();
+      return;
+    }
+
+    if (event.inputType === "deleteContentBackward") {
+      if (current.previousElementSibling) {
+        current.previousElementSibling.focus();
+      }
+    } else if (current.nextElementSibling) {
+      current.nextElementSibling.focus();
+    }
+  });
+});
+
 window.addEventListener("paste", (event) => {
   const clipboardData = event.clipboardData || window.clipboardData;
   const pastedData = clipboardData.getData("text").trim();
-
-  console.log(pastedData);
 
   if (/^\d{4}$/.test(pastedData)) {
     otpInputs.forEach((input, index) => {
